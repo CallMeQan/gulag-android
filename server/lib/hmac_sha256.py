9e042aa698327126d5bdb6f4e59a47e9854b36aa
Châu Minh Quân <104240214@student.vgu.edu.vn>
2025-04-01 21:08:11 +0700

    [Remove]: move server folder to new repository

--- server/lib/hmac_sha256.py
+++
@@ -1,3 +0,0 @@
-import hmac, hashlib
-def generate_api_key(secret_key: str) -> str:
-    return hmac.new(secret_key.encode(), digestmod=hashlib.sha256).hexdigest()
--- server/tmp_storange.py
+++
@@ -1,4 +0,0 @@
-import threading
-
-storage_lock = threading.Lock()
-tmp_storage = []
