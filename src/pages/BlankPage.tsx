b10533989e15e6fbb3435f2e4f0985ad7825f65f
Châu Minh Quân <104240214@student.vgu.edu.vn>
2025-04-15 00:28:01 +0700

    [Remove]: unused pages

--- src/pages/BlankPage.tsx
+++
@@ -1,7 +0,0 @@
-export default function BlankPage() {
-    return (
-        <div className="flex items-center justify-center min-h-screen bg-black">
-            <h1 className="text-3xl font-bold text-red-600">Please restart the app</h1>
-        </div>
-    );
-}
